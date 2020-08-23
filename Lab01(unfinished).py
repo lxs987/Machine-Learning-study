8db7d2df6c6137223d0a558589c8949342587733
김진경 <53249738+lxs987@users.noreply.github.com>
2020-08-23 21:06:03 +0900

    Add Test Message

--- Lab01(unfinished).py
+++ Lab01(unfinished).py
@@ -2,8 +2,10 @@ lst = ['a', 'ab', 'abc', 'abcd']
 newlst = []
 strLuckyData = "LuckyData"
 n = 10
+strTestMessage = "hihi"
 for i in range(len(lst)):
     newlst.append(lst[i])
 
 print(newlst)
 print(strLuckyData)
+print(strTestMessage)
